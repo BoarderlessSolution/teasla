a0ccf6c5b1e1202b7d546bcb3c09bb8a5df025de
simon <simon@gmail.com>
2023-01-29 00:20:16 +0000

    conflict resolved

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
 deploy java application
+deploy python apps
+deploy in the dev env
+deployment5 master
+deployment4
+deploy to staging env
+echo good job
+tested and accepted
+3f3289b (tested and approved by the client QA team)
+conflict resolved
